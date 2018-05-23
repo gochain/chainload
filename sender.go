ef45d2f5259846980d54b32a4fe614a183de9720
jmank88 <jmank88@gmail.com>
2018-05-23 18:21:43 -0500

    gas price jitter lower bound

--- sender.go
+++ sender.go
@@ -218,9 +218,12 @@ func (s *Sender) updateGasPrice(ctx context.Context) {
 func (s *Sender) send(ctx context.Context) {
 	recv := s.recv[int(s.nonce)%len(s.recv)]
 	gp := s.gasPrice.Uint64()
-	gasPrice := new(big.Int).SetUint64(randBetween(gp*8/10, gp*12/10))
+	gp = randBetween(gp*8/10, gp*12/10)
+	if gp == 0 {
+		gp = 1
+	}
 	amount := new(big.Int).SetUint64(randBetween(config.amount, 2*config.amount))
-	tx := types.NewTransaction(s.nonce, recv, amount, randBetween(config.gas, 2*config.gas), gasPrice, nil)
+	tx := types.NewTransaction(s.nonce, recv, amount, randBetween(config.gas, 2*config.gas), new(big.Int).SetUint64(gp), nil)
 	t := time.Now()
 	tx, err := s.AccountStore.SignTx(*s.acct, tx)
 	if err != nil {
