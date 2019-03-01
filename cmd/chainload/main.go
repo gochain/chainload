44d764230a7807eee306eb81e45966b174a9d1e6
jmank88 <jmank88@gmail.com>
2019-03-01 14:40:19 -0600

    flag fix

--- cmd/chainload/main.go
+++ cmd/chainload/main.go
@@ -24,18 +24,18 @@ func init() {
 var config = chainload.Config{}
 
 func init() {
-	flag.Uint64Var(&config.Id, "Id", 1234, "Id")
+	flag.Uint64Var(&config.Id, "id", 1234, "Id")
 	flag.StringVar(&config.UrlsCSV, "urls", "http://localhost:8545", "csv of urls")
-	flag.IntVar(&config.TPS, "TPS", 1, "transactions per second")
-	flag.IntVar(&config.Senders, "Senders", 0, "total number of concurrent Senders/accounts - defaults to TPS")
-	flag.DurationVar(&config.Cycle, "Cycle", 5*time.Minute, "how often to Cycle a sender's account")
-	flag.DurationVar(&config.Duration, "Duration", 0, "duration to run - omit for unlimited")
-	flag.StringVar(&config.Password, "Password", "#go@chain42", "passphrase to unlock accounts")
-	flag.Uint64Var(&config.Gas, "Gas", 200000, "Gas (approximate)")
-	flag.Uint64Var(&config.Amount, "Amount", 10, "tx Amount (approximate)")
+	flag.IntVar(&config.TPS, "tps", 1, "transactions per second")
+	flag.IntVar(&config.Senders, "senders", 0, "total number of concurrent Senders/accounts - defaults to TPS")
+	flag.DurationVar(&config.Cycle, "cycle", 5*time.Minute, "how often to Cycle a sender's account")
+	flag.DurationVar(&config.Duration, "dur", 0, "duration to run - omit for unlimited")
+	flag.StringVar(&config.Password, "pass", "#go@chain42", "passphrase to unlock accounts")
+	flag.Uint64Var(&config.Gas, "gas", 200000, "Gas (approximate)")
+	flag.Uint64Var(&config.Amount, "amount", 10, "tx Amount (approximate)")
 	flag.StringVar(&config.PprofAddr, "pprof", ":6060", "pprof addr")
 	flag.BoolVar(&config.Verbose, "v", false, "Verbose logging")
-	flag.DurationVar(&config.Variable, "Variable", 30*time.Second, "Variable transaction rate")
+	flag.DurationVar(&config.Variable, "variable", 30*time.Second, "Variable transaction rate")
 }
 
 func main() {
