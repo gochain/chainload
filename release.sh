4110c50ca8a99c1dd799764a6a23ad82a2259aa5
jmank88 <jmank88@gmail.com>
2019-03-01 09:29:04 -0600

    change release process

--- release.sh
+++ release.sh
@@ -15,19 +15,13 @@ else
   exit 1
 fi
 
-version_file="version.go"
-# Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
-docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
-version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
+version=$(git tag --points-at HEAD)
+if [[ $version == "" ]]; then
+  version=$(git log -1 --format="%h")
+fi
 echo "Version: $version"
 
 make docker
-
-git add -u
-git commit -m "$image: $version release [skip ci]"
-git tag -f -a "$version" -m "version $version"
-git push
-git push origin $version
 
 # Push docker hub images
 docker tag $user/$image:latest $user/$image:$version
